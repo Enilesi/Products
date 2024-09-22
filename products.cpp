678bd11b7648bf272a048b9070c570952d830f0d
Enilesi <miriam.iseline@gmail.com>
2024-09-22 08:04:56 +0300

    add constructor InventoryManager , displayInventory method, .gitignore file

--- products.cpp
+++ products.cpp
@@ -5,10 +5,31 @@
 class InventoryManager{
     private:
         std::string item_name;
-        double price;
         int quantity;
+        double price;
+    public:
+        InventoryManager(std::string i_n="", int q=0, double p=0.0){
+            item_name=i_n;
+            price=p;
+            quantity=q;
+        }
+
+        void displayInventory() const {
+        std::cout << "Item: " << item_name 
+                  << ", Quantity: " << quantity 
+                  << ", Price:" << price << "$"<< std::endl;
+    }
 };
 
-int main() {
 
+
+
+int main() {
+    InventoryManager item1("Laptop", 10, 999.99);
+    InventoryManager item2("Smartphone", 25, 499.99);
+    InventoryManager item3("Tablet", 15, 299.99);
+
+    item1.displayInventory();
+    item2.displayInventory();
+    item3.displayInventory();
 }
