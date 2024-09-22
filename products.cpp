c59daa33032b3088cb748c0b3fcc24b01b3df20a
Enilesi <miriam.iseline@gmail.com>
2024-09-22 07:36:14 +0300

    init repository

---
+++ products.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+class InventoryManager{
+    private:
+        std::string item_name;
+        double price;
+        int quantity;
+};
+
+int main() {
+
+}
